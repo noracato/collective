00d840b529c127e5d245caedaf3df262db742fb7
noracato <ncschinkel@gmail.com>
2023-05-18 11:55:16 +0200

    First draft app for users

---
+++ beat.js
@@ -0,0 +1,139 @@
+let sounds = [
+    {
+        "keepPlaying": false,
+        "sound": 'aa aa',
+        "voice": "es-ES",
+        "icon": "❖"
+    },
+    {
+        "keepPlaying": false,
+        "sound": 'ксж',
+        "voice": "ru-RU",
+        "icon": "✶"
+    },
+    {
+        "keepPlaying": false,
+        "sound": 'iao',
+        "voice": "it-IT",
+        "icon": "△"
+    },
+    {
+        "keepPlaying": false,
+        "sound": 'rrr',
+        "voice": "ro-RO",
+        "icon": "✽"
+    },
+    {
+        "keepPlaying": false,
+        "sound": 'ввв',
+        "voice": "ru-RU",
+        "icon": "◎"
+    },
+    {
+        "keepPlaying": false,
+        "sound": '我',
+        "voice": "zh-HK",
+        "icon": "❀"
+    },
+    {
+        "keepPlaying": false,
+        "sound": '브',
+        "voice": "ko-KR",
+        "icon": "⦿"
+    },
+    {
+        "keepPlaying": false,
+        "sound": 'th',
+        "voice": "ja-JA",
+        "icon": "□",
+    },
+    {
+        "keepPlaying": false,
+        "sound": 'κκ',
+        "voice": "el-GR",
+        "icon": "✸"
+    },
+]
+
+const soundsPerRow = 3;
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+let speed = 1000;
+$('#speedSlider').change(function (e) {
+    speed = e.target.value;
+})
+
+buildAll();
+
+function play(soundIndex){
+    playByText(
+        sounds[soundIndex]["voice"],
+        sounds[soundIndex]["sound"],
+        sounds[soundIndex]["volume"]
+    )
+}
+
+const hold = async (soundIndex) => {
+    // Check if already playing
+    if (sounds[soundIndex]["keepPlaying"]){
+        return;
+    }
+
+    sounds[soundIndex]["keepPlaying"] = true;
+    while(sounds[soundIndex]["keepPlaying"]){
+        play(soundIndex);
+        await delay(speed);
+    }
+};
+
+function stopPlaying(soundIndex){
+    sounds[soundIndex]["keepPlaying"] = false;
+}
+
+
+function buildAll() {
+    var container = $('#buttons');
+
+    for (var i = 0; i < sounds.length; i+=soundsPerRow){
+        container.append(buildSoundRow(i, i+soundsPerRow));
+    }
+}
+
+function buildSoundRow(startIndex, endIndex){
+    let row = $('<div></div>');
+    row.addClass("row blacks");
+
+    for (var i = startIndex; i < endIndex; i++){
+        row.append(buildSoundButton(i));
+    }
+
+    return row;
+}
+
+
+function buildSoundButton(index) {
+    let container = $('<div></div>');
+    let ball = $('<a><p>'+ sounds[index]["icon"] +'</p></a>');
+    ball.click(function() { play(index); })
+
+    let controls = $('<div></div>');
+    controls.addClass("row controls");
+    controls.click(function () {
+        if (controls.hasClass("on")){
+            controls.removeClass('on');
+            stopPlaying(index);
+        } else {
+            controls.addClass('on');
+            hold(index);
+        }
+    });
+
+    let holdControl = $('<div>REPEAT</div>');
+
+    controls.append(holdControl);
+    container.append(ball);
+    container.append(controls);
+
+    return container;
+}
---
+++ synthesis.js
@@ -0,0 +1,99 @@
+let _speechSynth
+let _voices
+const _cache = {}
+
+/**
+ * retries until there have been voices loaded. No stopper flag included in this example.
+ * Note that this function assumes, that there are voices installed on the host system.
+ */
+
+function loadVoicesWhenAvailable (onComplete = () => {}) {
+  _speechSynth = window.speechSynthesis
+  const voices = _speechSynth.getVoices()
+
+  if (voices.length !== 0) {
+    _voices = voices
+
+    onComplete()
+  } else {
+    return setTimeout(function () { loadVoicesWhenAvailable(onComplete) }, 100)
+  }
+}
+
+/**
+ * Returns the first found voice for a given language code.
+ */
+
+function getVoices (locale) {
+  if (!_speechSynth) {
+    throw new Error('Browser does not support speech synthesis')
+  }
+  if (_cache[locale]) return _cache[locale]
+
+  _cache[locale] = _voices.filter(voice => voice.lang === locale)
+  return _cache[locale]
+}
+
+/**
+ * Speak a certain text
+ * @param locale the locale this voice requires
+ * @param text the text to speak
+ * @param onEnd callback if tts is finished
+ */
+
+function playByText (locale, text, rate, onEnd) {
+  const voices = getVoices(locale)
+
+
+  // TODO load preference here, e.g. male / female etc.
+  // TODO but for now we just use the first occurrence
+  const utterance = new window.SpeechSynthesisUtterance()
+  utterance.voice = voices[0]
+  utterance.pitch = 1
+  utterance.voiceURI = 'native'
+
+  if (rate){
+    utterance.rate = rate;
+  } else {
+    utterance.rate = 5;
+  }
+
+  utterance.volume = 1
+
+
+  utterance.text = text
+  utterance.lang = locale
+
+  if (onEnd) {
+    utterance.onend = onEnd
+  }
+
+
+//   _speechSynth.cancel() // cancel current speak, if any is running
+  _speechSynth.speak(utterance)
+}
+
+// on document ready
+loadVoicesWhenAvailable(function () {
+ console.log("loaded")
+})
+
+function inAllVoices(text) {
+    for (let i =0; i < _voices.length; i++){
+        setTimeout(function() {playByText(_voices[i].lang, text)}, i * 1000)
+    }
+}
+
+function inRandomVoice(text, times) {
+    var voice;
+    for (let i =0; i < times; i++){
+        setTimeout(function() {
+            voice = _voices[Math.floor(Math.random() * _voices.length)];
+            playByText(voice.lang, text)
+        }, i * 1000)
+    }
+}
+
+// function speak () {
+//   setTimeout(() => playByText("en-US", "Hello, world"), 300)
+// }
